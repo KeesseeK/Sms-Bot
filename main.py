4a2ddf081d4df6cdfce5758175397356f2546ddb
Krzesimir Kłujszo <107472657+KeesseeK@users.noreply.github.com>
2022-12-27 15:11:32 +0000

    working on numbers extracting

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import csv
 
 url = 'https://www.morizon.pl/mieszkania/warszawa/praga-poludnie/?ps%5Bowner%5D%5B0%5D=4'
 
-response = requests.get(url)
+urls_response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(urls_response.text, 'html.parser')
 
 links = soup.find_all('a', class_='property_link property-url')
 
@@ -17,11 +17,9 @@ with open('morizon_links.csv', 'w') as csv_file:
 
 
 
-response = requests.get(url)
+numbers_response = requests.get(links)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-
-links = soup.find_all('a', class_='property_link property-url')
+numbers = soup.find_all('a', class_='property_link property-url')
 
 with open('morizon_links.csv', 'w') as csv_file:
     writer = csv.writer(csv_file)
