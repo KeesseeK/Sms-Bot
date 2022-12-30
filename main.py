b27b55c8746f838e2861662e23083a9e2b9959c3
Krzesimir Kłujszo <107472657+KeesseeK@users.noreply.github.com>
2022-12-30 10:28:03 +0000

    trying to figure out why bs dont see phone numbers

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from bs4 import BeautifulSoup
 import csv
 
 url = 'https://www.morizon.pl/mieszkania/warszawa/praga-poludnie/?ps%5Bowner%5D%5B0%5D=4'
-
 urls_response = requests.get(url)
-
 soup = BeautifulSoup(urls_response.text, 'html.parser')
+soup_links = soup.find_all('a', class_='property_link property-url')
+links = []
 
-links = soup.find_all('a', class_='property_link property-url')
-
-with open('morizon_links.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for link in links:
-        writer.writerow([link['href']])
-
+with open('morizon_links.csv', 'w') as morizon_links:
+    writer = csv.writer(morizon_links)
+    for soup_link in soup_links:
+        writer.writerow([soup_link['href']])
+        links.append(soup_link['href'])
 
 
-numbers_response = requests.get(links)
 
-numbers = soup.find_all('a', class_='property_link property-url')
+numbers_response = requests.get(links[0])
+print(numbers_response.text)
+soup2 = BeautifulSoup(numbers_response.text, 'html.parser')
+numbers = soup2.find_all('span ', class_='phone hidden')
 
-with open('morizon_links.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    for link in links:
-        writer.writerow([link['href']])
+with open('morizon_numbers.csv', 'w') as morizon_numbers:
+    writer = csv.writer(morizon_numbers)
+    for number in numbers:
+        writer.writerow([number])
