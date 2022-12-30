f976a041cdf9ecfbd3ae5ba53bd856e31c391348
Krzesimir Kłujszo <107472657+KeesseeK@users.noreply.github.com>
2022-12-30 12:59:35 +0000

    writing functions

--- main.py
+++ main.py
@@ -9,40 +9,26 @@ numbers = []
 def get_links(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    for link in soup.find_all('a'):
-        if link.get('href'):
-            links.append(link.get('href'))
+    soup_links = soup.find_all('a', class_='property_link property-url')
+  
+    with open('morizon_links.csv', 'w') as morizon_links:
+      writer = csv.writer(morizon_links)
+      for soup_link in soup_links:
+          writer.writerow([soup_link['href']])
+          links.append(soup_link['href'])
 
 def get_numbers(links):
     for link in links:
-      r = requests.get(link)
-      soup = BeautifulSoup(r.text, 'html.parser')
-      for link in soup.find_all('span', class_='number')
+      url2 = link
+      urls_response = requests.get(url2)
+      soup = BeautifulSoup(urls_response.text, 'html.parser')
+      soup_links = soup.find_all('span', class_='phone hidden')
+      numbers.append(soup_links[0].text)
 
-                              
+      with open('morizon_links2.csv', 'w') as morizon_links2:
+        writer = csv.writer(morizon_links2)
+        for number in numbers:
+            writer.writerow([number])
 
-urls_response = requests.get(url)
-soup = BeautifulSoup(urls_response.text, 'html.parser')
-soup_links = soup.find_all('a', class_='property_link property-url')
-links = []
-numbers = []
-
-with open('morizon_links.csv', 'w') as morizon_links:
-    writer = csv.writer(morizon_links)
-    for soup_link in soup_links:
-        writer.writerow([soup_link['href']])
-        links.append(soup_link['href'])
-
-for link in links:
-  url2 = link
-  urls_response = requests.get(url2)
-  soup = BeautifulSoup(urls_response.text, 'html.parser')
-  soup_links = soup.find_all('span', class_='phone hidden')
-  numbers.append(soup_links[0].text)
-
-with open('morizon_links2.csv', 'w') as morizon_links2:
-    writer = csv.writer(morizon_links2)
-    for number in numbers:
-        writer.writerow([number])
-
-print(numbers)
+get_links(url)
+get_numbers(links)
