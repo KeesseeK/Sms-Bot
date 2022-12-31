d5f754c500f0168607c60c93a249b84985044eee
Krzesimir Kłujszo <107472657+KeesseeK@users.noreply.github.com>
2022-12-31 15:26:46 +0000

    turned into csv, trying to store data in right order

--- main.py
+++ main.py
@@ -2,40 +2,47 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = 'https://www.morizon.pl/mieszkania/warszawa/goclaw/?ps%5Bprice_from%5D=370000&ps%5Bprice_to%5D=600000&ps%5Bowner%5D%5B0%5D=4'
-links = []
-numbers = []
+urls = ['https://www.morizon.pl/mieszkania/warszawa/goclaw/?ps%5Bprice_from%5D=370000&ps%5Bprice_to%5D=600000&ps%5Bowner%5D%5B0%5D=4']
 
-def get_links(url):
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    soup_links = soup.find_all('a', class_='property_link property-url')
+
+
+class Scrape:
+  def get_links(self, urls):
+    for url in urls:
+        url_response = requests.get(url)
+        soup = BeautifulSoup(url_response.text, 'html.parser')
+        soup_links = soup.find_all('a', class_='property_link property-url')
+        links = []
+        for soup_link in soup_links:
+            links.append(soup_link['href'])
+    print(links)
+    return links
   
-    with open('morizon_links.csv', 'w') as morizon_links:
-      writer = csv.writer(morizon_links)
-      for soup_link in soup_links:
-          writer.writerow([soup_link['href']])
-          links.append(soup_link['href'])
-
-def get_numbers(links):
+  
+  def get_numbers(self, links):
+    numbers = []
     for link in links:
-      url2 = link
-      urls_response = requests.get(url2)
-      soup = BeautifulSoup(urls_response.text, 'html.parser')
-      soup_links = soup.find_all('span', class_='phone hidden')
-      numbers.append(soup_links[0].text)
+      number_response = requests.get(link)
+      soup = BeautifulSoup(number_response.text, 'html.parser')
+      soup_number= soup.find_all('span', class_='phone hidden')
+      numbers.append(soup_number[0].text)
+    print(numbers)
+    return numbers
 
-      with open('numbers.csv', 'w') as numberscsv:
-        writer = csv.writer(numberscsv)
-        for number in numbers:
-            writer.writerow([number])
-          
-def writing_to_csv(numbers, links):
-    with open('data.csv', 'w') as d:
-      writer = csv.writer(d)
-      writer.writerows(zip(numbers,links))
+class Data:
+  def write_to_csv(self, links, numbers):
+    try:
+      with open("data.csv", 'w') as csvfile:
+        headers = ["Number","Link"]
+        writer = csv.DictWriter(csvfile, fieldnames=headers)
+        writer.writeheader()
+        writer.writerow(links)
+        writer.writerow(numbers)
+    except IOError:
+      print("I/O error")
 
 
-get_links(url)
-get_numbers(links)
-writing_to_csv(numbers, links)
+if __name__ == '__main__':
+  scrape = Scrape()
+  data = Data()
+  data.write_to_csv(scrape.get_numbers(scrape.get_links(urls)))
